bfece97579f204ed3876a410f617eae73c9dda7c
jgonzalezmorente <jgonzalezmorente@gmail.com>
2025-05-18 20:04:09 +0200

    Primer commit

---
+++ 01-variables.py
@@ -0,0 +1,10 @@
+
+name = 'Ricardo'
+full_name = 'Ricardo Cuéllar'
+
+_private_name = 'Richie'
+PI = 3.1416
+GREET = 'Hola'
+
+print(name)
+print(full_name)
---
+++ 02-data_types.py
@@ -0,0 +1,17 @@
+# string
+name = 'Ricardo'
+last_name = 'Cuéllar'
+
+# int
+number = 2000
+age = 29
+waste = 25
+
+# floats
+decimal = 20.42
+temp = 27.3
+
+# boolean
+boolean = True # 1
+boolean_false = False # 0
+is_gamer = True
